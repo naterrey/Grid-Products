e8ff8813fb741e99a040b2fd2d83987b1218ec02
naterrey <naterrey@iitds.com>
2015-03-03 17:56:19 -0500

    Fixed "findlargestproductvertically" method and updated comments in main

--- src/packag/FindGridProduct.java
+++ src/packag/FindGridProduct.java
@@ -14,6 +14,12 @@ package packag;
 
 public class FindGridProduct 
 {
+	/*
+	 * Finds the largest product diagonally from left to, and both from bottom to top and top to bottom in a multidimensional square matrix
+	 * @param long[][] nums, int set
+	 * loops through the multidimensional array horizontally and multiply values vertically
+	 * @return largestproduct
+	 */
 	long findlargestproductdiagonally(long[][] nums, int set)
 	{
 		long tempproduct;
@@ -63,7 +69,7 @@ public class FindGridProduct
 					for(int k = 0; k <set; k++)
 					{
 						tempproduct = tempproduct * nums[j-k][k+count];
-						//System.out.println(nums[j-k][k+count]);//for testing
+						//System.out.print(nums[j-k][k+count] + " ");//for testing
 						//System.out.println(temp);
 						place++;
 					}
@@ -85,27 +91,34 @@ public class FindGridProduct
 		return largestproduct;
 		
 	}
+	/*
+	 * Finds the largest product vertically in a multidimensional square matrix
+	 * @param long[][] nums, int set
+	 * loops through the multidimensional array horizontally and multiply values vertically
+	 * @return largestproduct
+	 */
 	long findlargestproductvertically(long[][] nums, int set)
 	{
 		long tempproduct;
 		long largestproduct = 0;
 		int length = nums.length;
 		int width = nums[0].length;
-		//go through the multidimensional array horizontally and multiply values vertically
-		for(int l = width-set; l>=0;l--)
+		//go through the multidimensional array vertically and multiply values horizontally
+		for(int l = 0; l<=length-1;l++)
 		{
-			//System.out.println(l);
+			
 				int count = 0;
-				for(int j=l; count<=length-set; count++)
+				for(int j=l; count<=width-set; count++)
 				{
 					
-					//int place = 0;
+					int place = 0;
 					tempproduct = 1;
-					for(int k = 0; k<set; k++)
+					for(int k = 0; place<set&&k<length; k++)
 					{
-						tempproduct = tempproduct * nums[j][k+count];
-						//System.out.println(nums[j+k][k+count]);//for testing
+						tempproduct = tempproduct * nums[k+count][j];
+						//System.out.print(nums[k+count][j] + " ");//for testing
 						//System.out.println(temp);
+						place++;
 					}
 					//if (tempproduct > 1) System.out.println(tempproduct);//for testing
 					if (tempproduct > largestproduct) 
@@ -121,10 +134,14 @@ public class FindGridProduct
 				}	
 		
 		}
-		
-		
 		return largestproduct;
 	}
+	/*
+	 * Finds the largest product horizontally in a multidimensional square matrix
+	 * @param long[][] nums, int set
+	 * loops through the multidimensional array vertically and multiply values horizontally
+	 * @return largestproduct
+	 */
 	long findlargestproducthorizontally(long[][] nums, int set)
 	{
 		long tempproduct;
@@ -132,20 +149,22 @@ public class FindGridProduct
 		int length = nums.length;
 		int width = nums[0].length;
 		//go through the multidimensional array vertically and multiply values horizontally
-		for(int l = length; l>=0;l--)
+		System.out.println("hi");
+		for(int l = length-1; l>=0;l--)
 		{
 			//System.out.println(l);
 				int count = 0;
 				for(int j=l; count<=width-set; count++)
 				{
-					
-					//int place = 0;
+				
+					int place = 0;
 					tempproduct = 1;
-					for(int k = count; k<width; k++)
+					for(int k = count; place<set&&k<width; k++)
 					{
 						tempproduct = tempproduct * nums[j][k];
-						//System.out.println(nums[j+k][k+count]);//for testing
+						//System.out.print(nums[j][k] + " ");//for testing
 						//System.out.println(temp);
+						place++;
 					}
 					//if (tempproduct > 1) System.out.println(tempproduct);//for testing
 					if (tempproduct > largestproduct) 
@@ -161,6 +180,21 @@ public class FindGridProduct
 				}	
 		
 		}
+		return largestproduct;
+	}
+	//calls all other findlargestproduct methods and tests which one returns the largest number then returns that number
+	long findlargestproductoverall(long[][] nums, int set)
+	{	
+		long largestproduct =0;
+		long tempproduct = 0;
+		
+		tempproduct = findlargestproductdiagonally(nums, set);
+		if (tempproduct>largestproduct) largestproduct = tempproduct;
+		tempproduct = findlargestproducthorizontally(nums, set);
+		if (tempproduct>largestproduct) largestproduct = tempproduct;
+		tempproduct = findlargestproductvertically(nums, set);
+		if (tempproduct>largestproduct) largestproduct = tempproduct;
+		
 		return largestproduct;
 	}
 }
