0fb3e8a7bcd33e2c3ec2a184cb4fc46968c8005c
naterrey <naterrey@iitds.com>
2015-03-02 23:39:56 -0500

    Committing Eclipse Project Layout

---
+++ src/packag/FindGridProduct.java
@@ -0,0 +1,18 @@
+package packag;
+/**
+* FindGridProduct(base Class) for assignment 7 Programming 2
+* Nathan Reynolds
+* Teacher: Professor Nicholson
+* Class: Computer Programming 2
+* Date: 2/26/15
+* Assignment Description: this is theBase class for a GitHub assignment to solve a projectEuler program in java and
+* 						 post it to a GitHub repository, then have someone else make changes to the main and commit and
+*						 push those changes to the repository
+****************************************************************************************************************
+*/
+
+
+public class FindGridProduct 
+{
+
+}
